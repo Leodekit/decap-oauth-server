3d1135a22507b0f1cd1752cfd34619698ae87f6f
Leodekit <leonides152@gmail.com>
2026-04-22 16:18:40 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+app.get("/auth", (req, res) => {
+  const redirect = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=repo`;
+  res.redirect(redirect);
+});
+
+app.get("/callback", async (req, res) => {
+  const code = req.query.code;
+
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: { Accept: "application/json" },
+    body: new URLSearchParams({
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      code,
+    }),
+  });
+
+  const data = await response.json();
+
+  res.send(`
+    <script>
+      window.opener.postMessage(
+        'authorization:github:success:${data.access_token}',
+        '*'
+      );
+      window.close();
+    </script>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log("OAuth server running");
+});
